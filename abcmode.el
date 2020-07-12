59005e538bdbb480cb89528f9fcea5b05fbaac1c
Jörg Volbers <joerg@joergvolbers.de>
2020-07-12 21:44:45 +0200

    Added packae header

--- abcmode.el
+++ abcmode.el
@@ -1,7 +1,30 @@
-;; ABc minor mode:
-;;
+;;; abcmode.el --- autocorrect mistyping capital letters at the beginning of the word  -*- lexical-binding: t; -*-
 
-(define-minor-mode ABc-mode
+;; Copyright (C) 2020  
+
+;; Author:  <joerg@joergvolbers.de>
+;; Keywords: convenience
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; 
+
+;;; Code:
+
+(define-minor-mode abc-mode
   "Toggle ABc mode. Minor mode enabling automatic correction of
 misspelled capital words, i.e. WIlliam -> William."
   ;; inital value:
@@ -11,22 +34,22 @@ misspelled capital words, i.e. WIlliam -> William."
   ;; bindings:
   nil
   ;; body:
-  (if (not ABc-mode)
-      (ABc-remove-change-hooks)
-    (ABc-setup-change-hooks)))
+  (if (not abc-mode)
+      (abc-remove-change-hooks)
+    (abc-setup-change-hooks)))
 
-(defun ABc-setup-change-hooks ()
+(defun abc-setup-change-hooks ()
   (interactive)
-  (add-hook 'after-change-functions 'ABc-after-change-function  nil  t))
+  (add-hook 'after-change-functions 'abc-after-change-function  nil  t))
 
-(defun ABc-remove-change-hooks ()
+(defun abc-remove-change-hooks ()
   (interactive)
-  (remove-hook 'after-change-functions  'ABc-after-change-function  t))
+  (remove-hook 'after-change-functions  'abc-after-change-function  t))
 
 (defvar abc--regexp "\\b[[:upper:]][[:upper:]]\\([[:lower:]]+\\)\\b"
   "Regexp used to identify these words which ABC-modes is supposed to correct.")
 
-(defun ABc-after-change-function (start end length)
+(defun abc-after-change-function (start end length)
  (when  (zerop length) ;; insertion
    (save-match-data
      (let* ((point (point))
@@ -36,38 +59,6 @@ misspelled capital words, i.e. WIlliam -> William."
 	 (goto-char match)
 	 (capitalize-word 1))
        (goto-char point)))))
-
-;; --------------------------------------------------------------------------------
-;; Ab hier Tests.
-
-(defun abc--test-for-x (regexp string-to-test)
-  (let ((case-fold-search nil))
-  (insert (format "For string '%s': %s\t\n" 
-		  string-to-test 
-		  (if (string-match regexp string-to-test) 
-		      (if (match-string 1 string-to-test)
-			  (capitalize (match-string 0 string-to-test))
-			(downcase (match-string 0 string-to-test)))
-		    (concat string-to-test " (no match)"))))))
-
-(defun abc--testsuite ()
-  (interactive)
-  (let ((tempbufname "**abc testsuite***"))
-  (with-output-to-temp-buffer tempbufname
-    (set-buffer tempbufname)
-    (abc--test-for-x abc--regexp "Normal")
-    (abc--test-for-x abc--regexp "BLatt")
-    (abc--test-for-x abc--regexp "BLatts")
-    (abc--test-for-x abc--regexp "BLätter")    
-    (abc--test-for-x abc--regexp "BLätters")    
-    (abc--test-for-x abc--regexp "CDs")
-    (abc--test-for-x abc--regexp "PDFs")
-    (abc--test-for-x abc--regexp "kAnt")
-    (abc--test-for-x abc--regexp "McDowell")
-    (abc--test-for-x abc--regexp "MCDowell")    
-    (abc--test-for-x abc--regexp "GROSS")
-    (abc--test-for-x abc--regexp "nichts")
-    (abc--test-for-x abc--regexp "12XYaa"))))
-    
-
+ 
 (provide 'abcmode)
+;;; abcmode.el ends here
