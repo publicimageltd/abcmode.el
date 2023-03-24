ea87d56b4371c83cee94187a043341a735c9137b
Jörg Volbers <joerg@joergvolbers.de>
2023-03-24 22:54:01 +0100

    Cleanup code

--- abcmode.el
+++ abcmode.el
@@ -1,6 +1,6 @@
 ;;; abcmode.el --- autocorrect mistyping capital letters at the beginning of the word  -*- lexical-binding: t; -*-
 
-;; Copyright (C) 2020  
+;; Copyright (C) 2020
 
 ;; Author:  <joerg@joergvolbers.de>
 ;; Keywords: convenience
@@ -19,46 +19,33 @@
 ;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 ;;; Commentary:
-
-;; 
-
 ;;; Code:
 
 (define-minor-mode abc-mode
-  "Toggle ABc mode. Minor mode enabling automatic correction of
-misspelled capital words, i.e. WIlliam -> William."
-  ;; inital value:
-  nil
-  ;; indicator for mode line:
-  " ABc"
-  ;; bindings:
-  nil
-  ;; body:
+  "Toggle ABc mode.
+Minor mode enabling automatic correction of misspelled capital
+words, i.e. WIlliam -> William."
+  :lighter " ABc"
   (if (not abc-mode)
-      (abc-remove-change-hooks)
-    (abc-setup-change-hooks)))
-
-(defun abc-setup-change-hooks ()
-  (interactive)
-  (add-hook 'after-change-functions 'abc-after-change-function  nil  t))
-
-(defun abc-remove-change-hooks ()
-  (interactive)
-  (remove-hook 'after-change-functions  'abc-after-change-function  t))
+      (remove-hook 'after-change-functions  'abc-maybe-capitalize  t)
+    (add-hook 'after-change-functions 'abc-maybe-capitalize  nil  t)))
 
 (defvar abc--regexp "\\b[[:upper:]][[:upper:]]\\([[:lower:]]+\\)\\b"
   "Regexp used to identify these words which ABC-modes is supposed to correct.")
 
-(defun abc-after-change-function (start end length)
- (when  (zerop length) ;; insertion
+(defun abc-maybe-capitalize (start end length)
+  "Maybe capitalize the word at point.
+Use START, END and LENGTH as it is passed by `after-change-function'."
+  (ignore start end)
+  (when  (zerop length) ;; insertion
    (save-match-data
      (let* ((point (point))
-	    (case-fold-search nil) ;; do not ignore case
-	    (match (re-search-backward abc--regexp (point-at-bol) t)))
-       (when (and match (not (string= "s" (match-string 1)))) ;; Korrektur nur wenn kein abschließendes s
-	 (goto-char match)
-	 (capitalize-word 1))
+            (case-fold-search nil) ;; do not ignore case
+            (match (re-search-backward abc--regexp (point-at-bol) t)))
+       (when (and match (not (string= "s" (match-string 1))))
+         (goto-char match)
+         (capitalize-word 1))
        (goto-char point)))))
- 
+
 (provide 'abcmode)
 ;;; abcmode.el ends here
